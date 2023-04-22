1b954f8e505aec3eaddb4af082dc662527830d5a
MrDoctorASAP <>
2023-04-23 02:41:27 +0400

    clean up

--- backend/auth-service/src/main/java/com/messanger/auth/controller/dto/AuthenticationJwtToken.java
+++
@@ -1,9 +0,0 @@
-package com.messanger.auth.controller.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationJwtToken {
-    private final String username;
-    private final String token;
-}
